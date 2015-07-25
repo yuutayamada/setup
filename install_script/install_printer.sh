7047b2e0d264b261a70f053ecd2fa68f780c626a
Yuta Yamada <cokesboy@gmail.com>
2015-07-25 07:13:12 -0700

    Add installation script for printer

---
+++ install_script/install_printer.sh
@@ -0,0 +1,6 @@
+# Canon printers
+sudo apt-get install cnijfilter-common-64 cnijfilter-mg6100series-64
+# See network printer page of cups(localhost:631)
+sudo apt-get install snmp
+
+nmap IPADDRESS # <- this will find port of printer, I found lpd
